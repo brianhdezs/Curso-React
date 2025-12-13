87bf468743daeb17495869fde477c0d3abf299a0
Brian Flores <brianhdez824@gmail.com>
2025-12-12 23:21:39 -0600

    89-Manejo de cache

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -2,12 +2,20 @@ import { useState } from "react";
 import { getGifByQuery } from "../actions/get-gif-by-query.actions";
 import type { Gif } from "../interfaces/gif.interface";
 
+//cache simple en memoria
+const gifsCache: Record<string, Gif[]> = {};
+
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
-  const handleTermClicked = (term: string) => {
-    console.log({ term });
+  const handleTermClicked = async (term: string) => {
+    if (gifsCache[term]) {
+      setGifs(gifsCache[term]);
+      return;
+    }
+    const gifs = await getGifByQuery(term);
+    setGifs(gifs);
   };
 
   const handleSearch = async (query: string = "") => {
@@ -18,6 +26,9 @@ export const useGifs = () => {
 
     const gifs = await getGifByQuery(query);
     setGifs(gifs);
+
+    //aqui guardamos en cache
+    gifsCache[query] = gifs;
   };
 
   return {
