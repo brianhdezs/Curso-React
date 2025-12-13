79d3ff62724fe989577c5621ecdef73e7b9dcf42
Brian Flores <brianhdez824@gmail.com>
2025-12-12 23:30:35 -0600

    90. useRef - Mantener el valor entre re-renders

--- 03-gifs-app/src/gifs/hooks/useGifs.tsx
+++ 03-gifs-app/src/gifs/hooks/useGifs.tsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getGifByQuery } from "../actions/get-gif-by-query.actions";
 import type { Gif } from "../interfaces/gif.interface";
 
 //cache simple en memoria
-const gifsCache: Record<string, Gif[]> = {};
+//const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+
+  const gifsCache = useRef<Record<string, Gif[]>>({}).current; //usamos useRef para mantener la referencia del cache, .current para mantener el valor actual y evitar recrearlo en cada render
 
   const handleTermClicked = async (term: string) => {
     if (gifsCache[term]) {
